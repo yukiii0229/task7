580de6079007c56523f42b9fb857430acdf87c46
yukiii0229 <92935326+yukiii0229@users.noreply.github.com>
2022-01-11 09:56:17 +0900

    Update todoController.php

--- app/Http/Controllers/todoController.php
+++ app/Http/Controllers/todoController.php
@@ -5,16 +5,21 @@ namespace App\Http\Controllers;
 use App\Models\todo;
 use Illuminate\Http\Request;
 
+
 class todoController extends Controller
 {
     public function todo()
     {
         $items = todo::all();
         return view('todo', ['items' => $items]);
+        $data = [
+            'item'=>$todo,
+        ];
+        return view('todo', $data);
     }
     public function create(Request $request)
     {
-        $this->validate($request, Author::$rules);
+        $this->validate($request, todo::$rules);
         $form = $request->all();
         todo::create($form);
         return redirect('/');
