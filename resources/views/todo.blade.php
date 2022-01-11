b4603c63daa3c717683c1c1c979a247c47035928
yukiii0229 <92935326+yukiii0229@users.noreply.github.com>
2022-01-11 11:02:38 +0900

    Update todo.blade.php

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -1,43 +1,22 @@
-<style>
+<?php
 
-    ul{
-        list-style: none;
-        display: flex;
+namespace App\Http\Controllers;
+
+use App\Models\todo;
+use Illuminate\Http\Request;
+
+class todoController extends Controller
+{
+    public function todo()
+    {
+        return view('todo');
     }
-    li{
-        margin-left:10%;
+    public function create(Request $request)
+    {
+        $this->validate($request, todo::$rules);
+        $form = $request->all();
+        todo::create($form);
+        return redirect('/');
     }
-</style>
+}
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/sample.css">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
-<h1>todo list</h1>
-<input type="text" name="create">
-<input type="submit" name="button1" value="追加">
-
-<table>
- <tr>
-   <td>作成日</td>
-   <td>タスク名</td>
-   <td>更新</td>
-   <td>削除</td>
- </tr>
- <tr>
-   <td>{{ $item->created_at }}/td>
-   <td>{{ $txt->content}}</td>
-   <td></td>
-   <td></td>
- </tr>
-</table>
-</html>
