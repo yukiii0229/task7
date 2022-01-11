fe1dc7c58457b05d54937678ed6470769d1e1665
yukiii0229 <92935326+yukiii0229@users.noreply.github.com>
2022-01-11 16:02:04 +0900

    Update todo.blade.php

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -1,8 +1,25 @@
- <h1>todo list</h1>
-     <input type="text" name="create"><button>追加</button>
 
-  <table>
-  <tr>
+
+  <!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>COACHTECH</title>
+</head>
+
+<body>
+  <h1>todo list</h1>
+  <h1>{{$content}}</h1>
+  <form action="/todo/create" method="POST">
+    @csrf
+    <input type="text" name="content" >
+    <input type="submit"  >
+  </form>
+
+    <table>
+      <tr>
    <td>作成日</td>
    <td>タスク名</td>
    <td>更新</td>
@@ -16,3 +33,6 @@
  </tr>
 </table>
 
+</body>
+
+</html>
