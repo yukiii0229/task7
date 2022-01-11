c413c305d4df9e1367442b7d359f0be2d33f1e3f
yukiii0229 <92935326+yukiii0229@users.noreply.github.com>
2022-01-11 17:26:44 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,3 @@ use App\Http\Controllers\todoController;
 
 Route::get('/', [todoController::class, 'todo']);
 Route::post('/todo/create', [todoController::class, 'todo']);
-Route::post('/todo/update', [todoController::class, 'todo']);
-Route::post('/todo/delete', [todoController::class, 'todo']);
