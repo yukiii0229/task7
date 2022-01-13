901bceb9f79be2716edd970fd67c655495f1056e
yukiii0229 <92935326+yukiii0229@users.noreply.github.com>
2022-01-13 09:17:31 +0900

    Update todoController.php

--- app/Http/Controllers/todoController.php
+++ app/Http/Controllers/todoController.php
@@ -24,7 +24,6 @@ class todoController extends Controller
     }
     public function create(Request $request)
     {
-        dd($request->all());
         $this->validate($request, todo::$rules);
         $form = $request->all();
         todo::create($form);
